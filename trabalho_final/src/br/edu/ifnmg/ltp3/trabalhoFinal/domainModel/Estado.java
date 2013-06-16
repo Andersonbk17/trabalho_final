3f6ff93a74dbb20bfb25e3cacc46274c38ba2abe
emerson <emerson@emerson-epo>
2013-06-16 12:27:15 -0300

    Criação classe Estado.

---
+++ trabalho_final/src/br/edu/ifnmg/ltp3/trabalhoFinal/domainModel/Estado.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.ltp3.trabalhoFinal.domainModel;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author emerson
+ */
+public class Estado {
+    private int idEstado;
+    private String nome;
+    private boolean status;
+    
+    //Construtores
+
+    public Estado(int idEstado, String nome, boolean status) {
+        this.idEstado = idEstado;
+        this.nome = nome;
+        this.status = status;
+    }
+    
+    public Estado() {
+        this.idEstado = 0;
+        this.nome = "vazio";
+        this.status = true;
+    }
+    
+    //Metodos
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    //Hashcodes
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 61 * hash + this.idEstado;
+        hash = 61 * hash + Objects.hashCode(this.nome);
+        hash = 61 * hash + (this.status ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estado other = (Estado) obj;
+        if (this.idEstado != other.idEstado) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        return true;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return "Estado{" + "idEstado=" + idEstado + ", nome=" + nome + ", status=" + status + '}';
+    }
+    
+}

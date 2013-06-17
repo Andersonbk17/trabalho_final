05ed2621bd01e98a402df6c2afd9725661ac62c6
Modric <Modric@Modric-PC>
2013-06-17 19:34:49 -0300

    Criação de CampusDAO

---
+++ trabalho_final/src/br/edu/ifnmg/ltp3/trabalhoFinal/dataAccess/CampusDAO.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.ltp3.trabalhoFinal.dataAccess;
+
+import br.edu.ifnmg.ltp3.trabalhoFinal.domainModel.Campus;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author lewandowsky
+ */
+public class CampusDAO {
+    private ConexaoBanco conexao;
+    
+    public CampusDAO(){
+        conexao = new ConexaoBanco();
+    }
+    
+    
+    public boolean Salvar(Campus obj ) throws SQLException{
+        try{
+            if(obj.getIdCampus()== 0){
+                CallableStatement comando = conexao.getConexao().prepareCall("CALL sp_Campus(?)");
+                comando.setString(1, obj.getNome());
+                comando.execute();
+            }else{
+                CallableStatement comando1 = conexao.getConexao().prepareCall(""
+                        + "CALL sp_CampusAtualiza(?,?)");
+                comando1.setString(1, obj.getNome());
+                comando1.setInt(3, obj.getIdCampus());
+                comando1.executeUpdate();
+           }
+            return true;
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }finally{
+            conexao.getConexao().close();
+        }
+    
+    }
+    
+    public List<Campus> ListarTodos(int idCampos) throws SQLException{
+        try{
+            PreparedStatement comando = conexao.getConexao().prepareStatement(""
+                    + "SELECT * FROM vw_Campus WHERE idPessoa = ? AND status = 1 ");
+            comando.setInt(1, idCampos);
+            ResultSet consulta = comando.executeQuery();
+            List<Campus> lista = new LinkedList<>();
+            while(consulta.next()){
+                Campus novo = new Campus();
+                novo.setNome(consulta.getString("nome"));
+                novo.setIdCampus(consulta.getInt("idCampus"));
+                
+                lista.add(novo);
+            }
+            return lista;
+         }catch(SQLException ex){
+               ex.printStackTrace();
+               return null;
+        }finally{
+            conexao.getConexao().close();
+        }
+     }
+    
+    public boolean Apagar(int idCampus ) throws SQLException{
+        try{
+            PreparedStatement comando = conexao.getConexao().prepareStatement(""
+                    + "UPDATE Campus SET status = 0 WHERE idEmail = ?");
+            comando.setInt(1, idCampus);
+            comando.executeUpdate();
+            return true;
+        }catch(SQLException ex){
+           ex.printStackTrace();
+           return false;
+        }finally{
+            conexao.getConexao().close();
+        }
+    
+    
+    }
+}

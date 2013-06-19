4d3bfd91e022b1034a93870a5ab5b89e040cbb8b
Modric <Modric@Modric-PC>
2013-06-19 19:56:42 -0300

    Criado EstadoDAO

---
+++ trabalho_final/src/br/edu/ifnmg/ltp3/trabalhoFinal/dataAccess/EstadoDAO.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.ltp3.trabalhoFinal.dataAccess;
+
+import br.edu.ifnmg.ltp3.trabalhoFinal.domainModel.CursoArea;
+import br.edu.ifnmg.ltp3.trabalhoFinal.domainModel.Estado;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Modric
+ */
+public class EstadoDAO {
+    private ConexaoBanco conexao;
+    
+    public EstadoDAO(){
+        conexao = new ConexaoBanco();
+    }
+    
+    
+    public Estado Abrir(int idEstado) throws SQLException{
+        try{
+            PreparedStatement comando = conexao.getConexao().prepareStatement("SELECT * FROM Estado WHERE idEstado = ? AND status = 1");
+            comando.setInt(1, idEstado);
+            ResultSet consulta = comando.executeQuery();
+            Estado novo = null;
+            if(consulta.first()){
+                novo = new Estado();
+                novo.setIdEstado(idEstado);
+                novo.setNome(consulta.getString("nome"));
+             }
+            return novo;
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return null;
+        }finally{
+            conexao.getConexao().close();
+        }
+    
+    
+    }
+    
+    public List<Estado> ListarTodos() throws SQLException{
+        try{
+            PreparedStatement comando = conexao.getConexao().prepareStatement("SELECT * FROM Estado WHERE status = 1");
+            ResultSet consuta = comando.executeQuery();
+            List<Estado> lista = new LinkedList<>();
+            while(consuta.next()){
+                Estado novo = new Estado();
+                novo.setIdEstado(consuta.getInt("idEstado"));
+                novo.setNome(consuta.getString("nome"));
+                
+                lista.add(novo);
+            }
+            
+            return lista;
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return null;
+        }finally{
+            conexao.getConexao().close();
+        }
+        
+    
+    
+    
+    }
+}
